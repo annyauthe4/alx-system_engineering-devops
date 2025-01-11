44f8a131e9e928d8cf67e111cc33afb6fceed484
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-01-11 05:58:44 +0100

    Install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # Install flask from pip3 ensuring a specific version.
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip',
+package { 'python3-pip':
+  ensure   => 'installed',
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+  require => Package['python3-pip'],
 }
