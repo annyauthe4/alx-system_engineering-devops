7232a8058638220a9cdd40f37bc64b02e576d617
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-01-11 04:42:42 +0100

    Create a puppet file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This Puppet manifest creates a file at /tmp/school with specific permissions, ownership and content
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
