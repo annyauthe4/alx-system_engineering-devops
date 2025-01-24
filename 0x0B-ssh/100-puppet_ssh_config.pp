144d5711190554c925b92bdf50f609f2ca49caa2
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-01-24 17:02:22 +0100

    Puppet file configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,7 +2,7 @@
 
 file_line { 'Turn off passwd auth':
   ensure => 'present',
-  path   => '/etc/ssh/ssh_config,
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
