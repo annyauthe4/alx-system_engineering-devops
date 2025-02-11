11e42d400bef659b65eb8c30e51af31ed94232d8
Oluwaseun Otetumo <annyauthe@yahoo.com>
2025-02-11 05:46:02 +0100

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Export Employee todo record in JSON format.
+"""
+
+
+import requests
+import sys
+import json
+
+
+def export_in_json(employee_id):
+    """Export employee tasks in CSV format."""
+    REST_API = "https://jsonplaceholder.typicode.com"
+
+    # Fetch user data
+    user_response = requests.get(f"{REST_API}/users/{employee_id}")
+    if user_response.status_code != 200:
+        return
+    user_data = user_response.json()
+    username = user_data.get("username")
+
+    # Fetch user todos
+    todos_response = requests.get(f"{REST_API}/todos",
+                                  params={"userId": employee_id})
+    if todos_response.status_code != 200:
+        return
+    todos_data = todos_response.json()
+
+    # Convert to Comma-Separated-Values
+    json_file = f"{employee_id}.json"
+    json_data = {str(employee_id): [
+        {"task": task.get("title"), "completed": task.get("completed"),
+         "username": username} for task in todos_data
+    ]}
+    with open(json_file, mode="w") as f:
+        json.dump(json_data, f)
+    print(f"{json_file}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    export_in_json(employee_id)
